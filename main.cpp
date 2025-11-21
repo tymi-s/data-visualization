0cfca07656a096a08c18836ceefc28aab75633c5
tymi <tymonszyler@gmail.com>
2025-11-21 15:00:48 +0100

    smegma

--- main.cpp
+++ main.cpp
@@ -1,17 +1,39 @@
-// Nagłówki
 #define GLEW_STATIC
-#include  <GL/glew.h>
+#include <GL/glew.h>
 #include <SFML/Window.hpp>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include <iostream>
+using namespace std;
+using namespace glm;
+using namespace sf;
 
-// Kody shaderów
+void shaderCompilationStatus(GLuint shader, const char* shaderName) {
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE) {
+        cout << "\n BŁĄD: Niepoprawna kompilacja shadera o nazwie: " << shaderName;
+    }
+    else {
+        cout << "\n SUKCES: Kompilacja shadera: " << shaderName << " przebiegla poprawnie!\n";
+    }
+}
+
+// Shadery z obsługą 3D
 const GLchar* vertexSource = R"glsl(
 #version 150 core
-in vec2 position;
+in vec3 position;
 in vec3 color;
 out vec3 Color;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+
 void main(){
-Color = color;
-gl_Position = vec4(position, 0.0, 1.0);
+    Color = color;
+    gl_Position = proj * view * model * vec4(position, 1.0);
 }
 )glsl";
 
@@ -21,51 +43,102 @@ in vec3 Color;
 out vec4 outColor;
 void main()
 {
-outColor = vec4(Color, 1.0);
+    outColor = vec4(Color, 1.0);
 }
 )glsl";
 
 int main()
 {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////    zmienne programu
+
+    double yaw = -90.0;
+    double pitch = 0.0;
+    double lastX = 400.0;
+    double lastY = 300.0;
+    double sensitivity = 0.1;
+
     sf::ContextSettings settings;
     settings.depthBits = 24;
     settings.stencilBits = 8;
 
-    // Okno renderingu
-    sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Titlebar | sf::Style::Close, settings);
+    Window window(sf::VideoMode(800, 600), "OpenGL 3D Camera", sf::Style::Titlebar | sf::Style::Close, settings);
+    window.setMouseCursorGrabbed(true);
+    window.setMouseCursorVisible(false);
+    window.setFramerateLimit(20);
 
-    // Inicjalizacja GLEW
     glewExperimental = GL_TRUE;
     glewInit();
 
-    // Utworzenie VAO (Vertex Array Object)
+
+    glEnable(GL_DEPTH_TEST);
+
     GLuint vao;
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
-    // Utworzenie VBO (Vertex Buffer Object)
     GLuint vbo;
     glGenBuffers(1, &vbo);
 
-    GLfloat vertices[] = {
-        0.0f, 0.5f, 1.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f
+    // Wierzchołki sześcianu z kolorami
+    float vertices[] = {
+        // Pozycja          // Kolor
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f,
+
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 1.0f
     };
+
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Utworzenie i skompilowanie shadera wierzchołków
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexSource, NULL);
     glCompileShader(vertexShader);
+    shaderCompilationStatus(vertexShader, "vertex shader");
 
-    // Utworzenie i skompilowanie shadera fragmentów
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
     glCompileShader(fragmentShader);
+    shaderCompilationStatus(fragmentShader, "fragment shader");
 
-    // Zlinkowanie obu shaderów w jeden wspólny program
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
@@ -73,39 +146,113 @@ int main()
     glLinkProgram(shaderProgram);
     glUseProgram(shaderProgram);
 
-    // Specyfikacja formatu danych wierzchołkowych
     GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
     glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 0);
 
     GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
     glEnableVertexAttribArray(colAttrib);
-    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
 
-    // Rozpoczęcie pętli zdarzeń
+
+    vec3 cameraPos = vec3(0.0f, 0.0f, 3.0f);
+    vec3 cameraFront = vec3(0.0f, 0.0f, -1.0f);
+    vec3 cameraUp = vec3(0.0f, 1.0f, 0.0f);
+    float obrot = 0.0f;
+
+
+    mat4 model = mat4(1.0f);
+    mat4 proj = perspective(radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+    // Lokalizacje uniform
+    GLint uniModel = glGetUniformLocation(shaderProgram, "model");
+    GLint uniView = glGetUniformLocation(shaderProgram, "view");
+    GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
+
+
+    glUniformMatrix4fv(uniProj, 1, GL_FALSE, value_ptr(proj));
+
+
+    Clock clock;
     bool running = true;
+
+
+    int frameCount = 0;
+    float fpsTimer = 0.0f;
+
     while (running) {
-        sf::Event windowEvent;
+        Event windowEvent;
+
+        float deltaTime = clock.restart().asSeconds();
+        float cameraSpeed = 2.5f * deltaTime;
+
         while (window.pollEvent(windowEvent)) {
-            switch (windowEvent.type) {
-                case sf::Event::Closed:
-                    running = false;
-                    break;
+            if (windowEvent.type == Event::Closed) {
+                running = false;
+            }
+
+            if (windowEvent.type == Event::MouseMoved) {
+                sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+
+                double xoffset = localPosition.x - lastX;
+                double yoffset = localPosition.y - lastY;
+                lastX = localPosition.x;
+                lastY = localPosition.y;
+
+                xoffset *= sensitivity;
+                yoffset *= sensitivity;
+
+                yaw += xoffset;
+                pitch -= yoffset;
+
+                if (pitch > 89.0f) pitch = 89.0f;
+                if (pitch < -89.0f) pitch = -89.0f;
+
+                vec3 front;
+                front.x = cos(radians(yaw)) * cos(radians(pitch));
+                front.y = sin(radians(pitch));
+                front.z = sin(radians(yaw)) * cos(radians(pitch));
+                cameraFront = normalize(front);
             }
         }
 
-        // Nadanie scenie koloru czarnego
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        if (Keyboard::isKeyPressed(Keyboard::W) || Keyboard::isKeyPressed(Keyboard::Up)) {
+            cameraPos += cameraSpeed * cameraFront;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::S) || Keyboard::isKeyPressed(Keyboard::Down)) {
+            cameraPos -= cameraSpeed * cameraFront;
+        }
 
-        // Narysowanie trójkąta
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        if (Keyboard::isKeyPressed(Keyboard::A) || Keyboard::isKeyPressed(Keyboard::Left)) {
+            cameraPos -= normalize(cross(cameraFront, cameraUp)) * cameraSpeed;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::D) || Keyboard::isKeyPressed(Keyboard::Right)) {
+            cameraPos += normalize(cross(cameraFront, cameraUp)) * cameraSpeed;
+        }
 
-        // Wymiana buforów
+        mat4 view = lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+        glUniformMatrix4fv(uniModel, 1, GL_FALSE, value_ptr(model));
+        glUniformMatrix4fv(uniView, 1, GL_FALSE, value_ptr(view));
+
+        glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
         window.display();
+
+        frameCount++;
+        fpsTimer += deltaTime;
+
+        if (fpsTimer >= 1.0f) {
+            string title = "OpenGL 3D Camera - FPS: " + to_string(frameCount);
+            window.setTitle(title);
+            frameCount = 0;
+            fpsTimer = 0.0f;
+        }
     }
 
-    // Czyszczenie
     glDeleteProgram(shaderProgram);
     glDeleteShader(fragmentShader);
     glDeleteShader(vertexShader);
